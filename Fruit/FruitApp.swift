5211b9ea978910ace82038c803f0d785a9d33cb8
Ibrahim diab <ibrahimdiab011@gmail.com>
2024-02-04 22:25:08 +0200

    Initial Commit

---
+++ Fruit/FruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitApp.swift
+//  Fruit
+//
+//  Created by Diab on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
